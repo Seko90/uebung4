ea9f48ffe002e48aceb51a0ab5f297a5b87301e9
Seko90 <Serkan.Altin90@gmail.com>
2014-02-01 19:31:22 +0100

    mit Farbe alles funktioniert

--- app/controllers/fizz_controller.rb
+++ app/controllers/fizz_controller.rb
@@ -1,9 +1,12 @@
 class FizzController < ApplicationController
-  def buzz
-  	@fizz_name="Izzy"
-  	@buzz_name="Bizzy"
-  	@fizz_number=2
-  	@buzz_number=4
-  	@max_number=100
-  end
+	def initialize
+		@fizz_name="Izzy"
+		@buzz_name="Bizzy"
+		@fizz_number=2
+		@buzz_number=4
+		@max_number=100
+	end
+
+	def buzz
+	end
 end
